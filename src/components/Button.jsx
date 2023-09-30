948673e261c267257ee33d5d8dc6d60f8d6dc0a4
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2023-09-30 12:23:47 +0530

    Special Offer Section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,19 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
   return (
     <button
-      className="flex justify-center 
-      items-center gap-2 px-7 py-4 border 
-      font-montserrat text-lg leading-none bg-coral-red 
-      rounded-full text-white border-coral-red"
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none hover:ml-2 hover:shadow-2xl
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full w-full"}`}
     >
       {label}
 
       {iconURL && <img
         src={iconURL}
         alt="Arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
+        className="ml-2 rounded-full w-5 h-5 hover:ml-4"
       />}
     </button>
   );
